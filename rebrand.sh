c7265b58ce56c58a160fdc9cfbacf01388aef770
Fuzail98 <102956978+Fuzail98@users.noreply.github.com>
2022-07-04 20:04:52 +0530

    Update rebrand.sh

--- rebrand.sh
+++ rebrand.sh
@@ -3,11 +3,14 @@
 echo "Rebranding zabbix to activeSONOR!!!"
 
 sudo cp icon-sprite.svg /usr/share/zabbix/assets/img/icon-sprite.svg 
-sudo cp favicon.ico /usr/share/zabbix/favicon.ico
 sudo cp blue-theme.css /usr/share/zabbix/assets/styles/blue-theme.css
 sudo cp page_header.php /usr/share/zabbix/include/page_header.php 
 sudo cp CBrandHelper.php /usr/share/zabbix/include/classes/helpers/CBrandHelper.php
 sudo cp general.login.php /usr/share/zabbix/include/views/general.login.php 
 sudo cp administration.system.info.php /usr/share/zabbix/app/partials/administration.system.info.php 
+
+convert favicon.png -define icon:auto-resize=256,64,48,32,16 favicon.ico
+
+sudo cp favicon.ico /usr/share/zabbix/favicon.ico
 
 echo "Rebranding has been done. Kindly refresh and/or clear your browser's cache to see the changes."
